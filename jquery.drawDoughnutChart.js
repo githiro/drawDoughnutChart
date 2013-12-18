179729691a32acb7222b6497dbb1d88a8a08b18a
'githiro' <'dev.hiroaki@gmail.com'>
2013-12-18 21:09:57 +0900

    Refactored2

--- jquery.drawDoughnutChart.js
+++ jquery.drawDoughnutChart.js
@@ -37,13 +37,13 @@
         summaryTitle: "TOTAL:",
         summaryTitleClass: "doughnutSummaryTitle",
         summaryNumberClass: "doughnutSummaryNumber",
-        beforeDraw: function(){  },
-        afterDrawed : function(){  },
-        onPathEnter : function(e,data){  },
-        onPathLeave : function(e,data){  }
+        beforeDraw: function() {  },
+        afterDrawed : function() {  },
+        onPathEnter : function(e,data) {  },
+        onPathLeave : function(e,data) {  }
       }, options),
       animationOptions = {
-        linear : function (t){
+        linear : function (t) {
           return t;
         },
         easeInOutExpo: function (t) {
@@ -51,7 +51,7 @@
           return (v>1) ? 1 : v;
         }
       },
-      requestAnimFrame = function(){
+      requestAnimFrame = function() {
         return window.requestAnimationFrame ||
           window.webkitRequestAnimationFrame ||
           window.mozRequestAnimationFrame ||
@@ -74,7 +74,7 @@
     //Draw base doughnut
     var baseDoughnutRadius = doughnutRadius + settings.baseOffset,
         baseCutoutRadius = cutoutRadius - settings.baseOffset;
-    var drawBaseDoughnut = function(){
+    var drawBaseDoughnut = function() {
         //Calculate values for the path.
         //We needn't calculate startRadius, segmentAngle and endRadius, because base doughnut doesn't animate.
         var startRadius = -1.570,// -Math.PI/2
@@ -125,7 +125,7 @@
     var $summaryTitle = $('<p class="' + settings.summaryTitleClass + '">' + settings.summaryTitle + '</p>').appendTo($summary);
     var $summaryNumber = $('<p class="' + settings.summaryNumberClass + '"></p>').appendTo($summary).css({opacity: 0});
 
-    for (var i = 0, len = data.length; i < len; i++){
+    for (var i = 0, len = data.length; i < len; i++) {
       segmentTotal += data[i].value;
       $paths[i] = $(document.createElementNS('http://www.w3.org/2000/svg', 'path'))
         .attr({
@@ -143,44 +143,44 @@
     //Animation start
     animationLoop(drawPieSegments);
 
-    function pathMouseEnter(e){
+    function pathMouseEnter(e) {
       var order = $(this).data().order;
       $tip.text(data[order].title + ": " + data[order].value)
           .fadeIn(200);
       settings.onPathEnter.apply($(this),[e,data]);
     }
-    function pathMouseLeave(e){
+    function pathMouseLeave(e) {
       $tip.hide();
       settings.onPathLeave.apply($(this),[e,data]);
     }
-    function pathMouseMove(e){
+    function pathMouseMove(e) {
       $tip.css({
         top: e.pageY + settings.tipOffsetY,
         left: e.pageX - $tip.width() / 2 + settings.tipOffsetX
       });
     }
-    function drawPieSegments (animationDecimal){
+    function drawPieSegments (animationDecimal) {
       var startRadius = -PI / 2,//-90 degree
           rotateAnimation = 1;
       if (settings.animation && settings.animateRotate) rotateAnimation = animationDecimal;//count up between0~1
 
-      drawDoughnutText(animationDecimal,segmentTotal);
+      drawDoughnutText(animationDecimal, segmentTotal);
 
-      $pathGroup[0].setAttribute("opacity", animationDecimal);
+      $pathGroup.attr("opacity", animationDecimal);
 
       //draw each path
-      for (var i = 0, len = data.length; i < len; i++){
-        var segmentAngle = rotateAnimation * ((data[i].value/segmentTotal) * (PI*2)),
-          endRadius = startRadius + segmentAngle,
-          largeArc = ((endRadius - startRadius) % (PI * 2)) > PI ? 1 : 0,
-          startX = centerX + cos(startRadius) * doughnutRadius,
-          startY = centerY + sin(startRadius) * doughnutRadius,
-          endX2 = centerX + cos(startRadius) * cutoutRadius,
-          endY2 = centerY + sin(startRadius) * cutoutRadius,
-          endX = centerX + cos(endRadius) * doughnutRadius,
-          endY = centerY + sin(endRadius) * doughnutRadius,
-          startX2 = centerX + cos(endRadius) * cutoutRadius,
-          startY2 = centerY + sin(endRadius) * cutoutRadius;
+      for (var i = 0, len = data.length; i < len; i++) {
+        var segmentAngle = rotateAnimation * ((data[i].value / segmentTotal) * (PI * 2)),
+            endRadius = startRadius + segmentAngle,
+            largeArc = ((endRadius - startRadius) % (PI * 2)) > PI ? 1 : 0,
+            startX = centerX + cos(startRadius) * doughnutRadius,
+            startY = centerY + sin(startRadius) * doughnutRadius,
+            endX2 = centerX + cos(startRadius) * cutoutRadius,
+            endY2 = centerY + sin(startRadius) * cutoutRadius,
+            endX = centerX + cos(endRadius) * doughnutRadius,
+            endY = centerY + sin(endRadius) * doughnutRadius,
+            startX2 = centerX + cos(endRadius) * cutoutRadius,
+            startY2 = centerY + sin(endRadius) * cutoutRadius;
         var cmd = [
           'M', startX, startY,//Move pointer
           'A', doughnutRadius, doughnutRadius, 0, largeArc, 1, endX, endY,//Draw outer arc path
@@ -193,48 +193,40 @@
       }
     }
 
-    function drawDoughnutText(animationDecimal, segmentTotal){
+    function drawDoughnutText(animationDecimal, segmentTotal) {
       $summaryNumber
         .css({opacity: animationDecimal})
         .text((segmentTotal * animationDecimal).toFixed(1));
     }
-    function animateFrame(cnt, drawData){
+    function animateFrame(cnt, drawData) {
       var easeAdjustedAnimationPercent =(settings.animation)? CapValue(easingFunction(cnt), null, 0) : 1;
       drawData(easeAdjustedAnimationPercent);
     }
-    function animationLoop(drawData){
+    function animationLoop(drawData) {
       var animFrameAmount = (settings.animation)? 1 / CapValue(settings.animationSteps, Number.MAX_VALUE, 1) : 1,
           cnt =(settings.animation)? 0 : 1;
-      requestAnimFrame(function(){
+      requestAnimFrame(function() {
           cnt += animFrameAmount;
           animateFrame(cnt, drawData);
-          if (cnt <= 1){
+          if (cnt <= 1) {
             requestAnimFrame(arguments.callee);
           } else {
             settings.afterDrawed.call($this);
           }
       });
     }
-    function Max(arr){
+    function Max(arr) {
       return Math.max.apply(null, arr);
     }
-    function Min(arr){
+    function Min(arr) {
       return Math.min.apply(null, arr);
     }
     function isNumber(n) {
       return !isNaN(parseFloat(n)) && isFinite(n);
     }
-    function CapValue(valueToCap, maxValue, minValue){
-      if(isNumber(maxValue)) {
-        if( valueToCap > maxValue ) {
-          return maxValue;
-        }
-      }
-      if(isNumber(minValue)){
-        if ( valueToCap < minValue ){
-          return minValue;
-        }
-      }
+    function CapValue(valueToCap, maxValue, minValue) {
+      if (isNumber(maxValue) && valueToCap > maxValue) return maxValue;
+      if (isNumber(minValue) && valueToCap < minValue) return minValue;
       return valueToCap;
     }
     return $this;
