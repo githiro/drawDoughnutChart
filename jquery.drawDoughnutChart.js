f84f1eba664e9a03e97e350c15ce00a7f4c67543
Adam Kecer <adam.kecer@gmail.com>
2015-07-01 13:25:48 +0200

    Rewritten, does not require jQuery now

--- jquery.drawDoughnutChart.js
+++ jquery.drawDoughnutChart.js
@@ -8,17 +8,25 @@
  * Released under the MIT license.
  *
  */
-;(function($, undefined) {
-  $.fn.drawDoughnutChart = function(data, options) {
-    var $this = this,
-      W = $this.width(),
-      H = $this.height(),
+ /*
+
+
+  var node = document.getElementById("div1");
+  var a = document.createAttribute("my_attrib");
+  a.value = "newVal";
+  node.setAttributeNode(a);
+
+ */
+  drawDoughnutChart = function(data, options) {
+    var $this = document.getElementById('doughnutChart'),
+      W = parseInt($this.clientWidth),
+      H = parseInt($this.clientHeight),
       centerX = W/2,
       centerY = H/2,
       cos = Math.cos,
       sin = Math.sin,
       PI = Math.PI,
-      settings = $.extend({
+      settings = {
         segmentShowStroke : true,
         segmentStrokeColor : "#0C1013",
         segmentStrokeWidth : 1,
@@ -45,7 +53,7 @@
         afterDrawed : function() {  },
         onPathEnter : function(e,data) {  },
         onPathLeave : function(e,data) {  }
-      }, options),
+      },
       animationOptions = {
         linear : function (t) {
           return t;
@@ -68,8 +76,13 @@
 
     settings.beforeDraw.call($this);
 
-    var $svg = $('<svg width="' + W + '" height="' + H + '" viewBox="0 0 ' + W + ' ' + H + '" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"></svg>').appendTo($this),
-        $paths = [],
+    var $svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    $svg.setAttribute("width", W);
+    $svg.setAttribute("height", H);
+    $svg.setAttribute("viewBox", '0 0 ' + W + ' ' + H);
+    $svg.setAttribute("xmlns:xlink", 'http://www.w3.org/1999/xlink');
+    $this.appendChild($svg);
+    var $paths = [],
         easingFunction = animationOptions[settings.animationEasing],
         doughnutRadius = Min([H / 2,W / 2]) - settings.edgeOffset,
         cutoutRadius = doughnutRadius * (settings.percentageInnerCutout / 100),
@@ -78,52 +91,61 @@
     //Draw base doughnut
     var baseDoughnutRadius = doughnutRadius + settings.baseOffset,
         baseCutoutRadius = cutoutRadius - settings.baseOffset;
-    $(document.createElementNS('http://www.w3.org/2000/svg', 'path'))
-      .attr({
-        "d": getHollowCirclePath(baseDoughnutRadius, baseCutoutRadius),
-        "fill": settings.baseColor
-      })
-      .appendTo($svg);
+    var tmpath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    tmpath.setAttribute("d", getHollowCirclePath(baseDoughnutRadius, baseCutoutRadius))
+    tmpath.setAttribute("fill", settings.baseColor);
+    $svg.appendChild(tmpath);
 
     //Set up pie segments wrapper
-    var $pathGroup = $(document.createElementNS('http://www.w3.org/2000/svg', 'g'));
-    $pathGroup.attr({opacity: 0}).appendTo($svg);
+    var $pathGroup = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+    $pathGroup.setAttribute('opacity',0);
+    $svg.appendChild($pathGroup);
 
     //Set up tooltip
     if (settings.showTip) {
-      var $tip = $('<div class="' + settings.tipClass + '" />').appendTo('body').hide(),
-          tipW = $tip.width(),
-          tipH = $tip.height();
+      var $tip = document.createElement('div');
+      $tip.setAttribute('class',settings.tipClass);
+      $tip.style.opacity = 0;
+      document.getElementsByTagName('body')[0].appendChild($tip);
+
+      var tipW = parseInt($tip.clientWidth),
+          tipH = parseInt($tip.clientHeight);
     }
 
     //Set up center text area
-    var summarySize = (cutoutRadius - (doughnutRadius - cutoutRadius)) * 2,
-        $summary = $('<div class="' + settings.summaryClass + '" />')
-                   .appendTo($this)
-                   .css({
-                     width: summarySize + "px",
-                     height: summarySize + "px",
-                     "margin-left": -(summarySize / 2) + "px",
-                     "margin-top": -(summarySize / 2) + "px"
-                   });
-    var $summaryTitle = $('<p class="' + settings.summaryTitleClass + '">' + settings.summaryTitle + '</p>').appendTo($summary);
-    $summaryTitle.css('font-size', getScaleFontSize( $summaryTitle, settings.summaryTitle )); // In most of case useless
-    var $summaryNumber = $('<p class="' + settings.summaryNumberClass + '"></p>').appendTo($summary).css({opacity: 0});
+    var summarySize = (cutoutRadius - (doughnutRadius - cutoutRadius)) * 2;
+    var $summary = document.createElement('div');
+    $summary.setAttribute('class', settings.summaryClass);
+    $summary.style.width = summarySize + 'px';
+    $summary.style.height = summarySize + 'px';
+    $summary.style.marginLeft = -(summarySize/2) + 'px';
+    $summary.style.marginTop = -(summarySize/2) + 'px';
+    $this.appendChild($summary);
+
+    var $summaryTitle = document.createElement('p');
+    $summaryTitle.setAttribute('class', settings.summaryTitleClass);
+    var textnode = document.createTextNode(settings.summaryTitle);
+    $summaryTitle.appendChild(textnode);
+    $summaryTitle.style.fontSize = getScaleFontSize( $summaryTitle, settings.summaryTitle );
+    $summary.appendChild($summaryTitle);
+
+    var $summaryNumber = document.createElement('p');
+    $summaryNumber.setAttribute('class', settings.summaryNumberClass);
+    $summaryNumber.style.opacity = 0;
+    $summary.appendChild($summaryNumber);
 
     for (var i = 0, len = data.length; i < len; i++) {
       segmentTotal += data[i].value;
-      $paths[i] = $(document.createElementNS('http://www.w3.org/2000/svg', 'path'))
-        .attr({
-          "stroke-width": settings.segmentStrokeWidth,
-          "stroke": settings.segmentStrokeColor,
-          "fill": data[i].color,
-          "data-order": i
-        })
-        .appendTo($pathGroup)
-        .on("mouseenter", pathMouseEnter)
-        .on("mouseleave", pathMouseLeave)
-        .on("mousemove", pathMouseMove)
-		.on("click", pathClick);
+      $paths[i] = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+      $paths[i].setAttribute('stroke-width', settings.segmentStrokeWidth);
+      $paths[i].setAttribute('stroke', settings.segmentStrokeColor);
+      $paths[i].setAttribute('fill', data[i].color);
+      $paths[i].setAttribute('data-order', i);
+      $paths[i].addEventListener("mouseenter", pathMouseEnter);
+      $paths[i].addEventListener("mouseleave", pathMouseLeave);
+      $paths[i].addEventListener("mousemove", pathMouseMove);
+      $paths[i].addEventListener("click", pathClick);
+      $pathGroup.appendChild($paths[i]);
     }
 
     //Animation start
@@ -156,146 +178,158 @@
         return cmd;
     };
     function pathMouseEnter(e) {
-      var order = $(this).data().order;
+      var order = parseInt(this.attributes['data-order'].value);
+      console.log('mouse entered', $tip);
       if (settings.showTip) {
-        $tip.text(data[order].title + ": " + data[order].value)
-            .fadeIn(200);
+        $tip.textContent = data[order].title + ": " + data[order].value;
+        //$tip.textContent.className = 'show';
+        $tip.style.opacity = 1;
       }
+      console.log($tip);
       if(settings.showLabel) {
-		  $summaryTitle.text(data[order].title).css('font-size', getScaleFontSize( $summaryTitle, data[order].title));
-          var tmpNumber = settings.shortInt ? shortKInt(data[order].value) : data[order].value;
-		  $summaryNumber.html(tmpNumber).css('font-size', getScaleFontSize( $summaryNumber, tmpNumber));
-	  }
-      settings.onPathEnter.apply($(this),[e,data]);
+        $summaryTitle.textContent = data[order].title;
+        $summaryTitle.style.fontSize = getScaleFontSize( $summaryTitle, data[order].title);
+        var tmpNumber = settings.shortInt ? shortKInt(data[order].value) : data[order].value;
+        $summaryNumber.innerHTML = tmpNumber;
+        $summaryNumber.style.fontSize = getScaleFontSize( $summaryNumber, tmpNumber);
+      }
+      settings.onPathEnter.apply(this,[e,data]);
     }
     function pathMouseLeave(e) {
-      if (settings.showTip) $tip.hide();
+      if (settings.showTip) $tip.style.opacity = 0;
       if(settings.showLabel) {
-		  $summaryTitle.text(settings.summaryTitle).css('font-size', getScaleFontSize( $summaryTitle, settings.summaryTitle));
-          var tmpNumber = settings.shortInt ? shortKInt(segmentTotal) : segmentTotal;
-		  $summaryNumber.html(tmpNumber).css('font-size', getScaleFontSize( $summaryNumber, tmpNumber));
-	  }
-      settings.onPathLeave.apply($(this),[e,data]);
+        $summaryTitle.textContent = settings.summaryTitle;
+        $summaryTitle.style.fontSize = getScaleFontSize( $summaryTitle, settings.summaryTitle);
+        var tmpNumber = settings.shortInt ? shortKInt(segmentTotal) : segmentTotal;
+        $summaryNumber.innerHTML = tmpNumber;
+        $summaryNumber.style.fontSize = getScaleFontSize( $summaryNumber, tmpNumber);
+      }
+      settings.onPathLeave.apply(this,[e,data]); //todo
     }
     function pathMouseMove(e) {
       if (settings.showTip) {
-        $tip.css({
-          top: e.pageY + settings.tipOffsetY,
-          left: e.pageX - $tip.width() / 2 + settings.tipOffsetX
-        });
+        $tip.style.top = e.pageY + settings.tipOffsetY + 'px';
+        $tip.style.left = e.pageX - parseInt($tip.clientWidth) / 2 + settings.tipOffsetX + 'px';
       }
     }
-	function pathClick(e){
-	var order = $(this).data().order;
-	  if (typeof data[order].action != "undefined")
-		  data[order].action();
-	}
-    function drawPieSegments (animationDecimal) {
-      var startRadius = -PI / 2,//-90 degree
-          rotateAnimation = 1;
-      if (settings.animation && settings.animateRotate) rotateAnimation = animationDecimal;//count up between0~1
+  function pathClick(e){
+    var order = parseInt(this.attributes['data-order'].value);
+    if (typeof data[order].action != "undefined")
+      data[order].action();
+  }
+  function drawPieSegments (animationDecimal) {
+    var startRadius = -PI / 2,//-90 degree
+        rotateAnimation = 1;
+    if (settings.animation && settings.animateRotate) rotateAnimation = animationDecimal;//count up between0~1
 
-      drawDoughnutText(animationDecimal, segmentTotal);
+    drawDoughnutText(animationDecimal, segmentTotal);
 
-      $pathGroup.attr("opacity", animationDecimal);
+    $pathGroup.setAttribute("opacity", animationDecimal);
 
-      //If data have only one value, we draw hollow circle(#1).
-      if (data.length === 1 && (4.7122 < (rotateAnimation * ((data[0].value / segmentTotal) * (PI * 2)) + startRadius))) {
-        $paths[0].attr("d", getHollowCirclePath(doughnutRadius, cutoutRadius));
-        return;
+    //If data have only one value, we draw hollow circle(#1).
+    if (data.length === 1 && (4.7122 < (rotateAnimation * ((data[0].value / segmentTotal) * (PI * 2)) + startRadius))) {
+      $paths[0].setAttribute("d", getHollowCirclePath(doughnutRadius, cutoutRadius));
+      return;
+    }
+    for (var i = 0, len = data.length; i < len; i++) {
+      var segmentAngle = rotateAnimation * ((data[i].value / segmentTotal) * (PI * 2)),
+          endRadius = startRadius + segmentAngle,
+          largeArc = ((endRadius - startRadius) % (PI * 2)) > PI ? 1 : 0,
+          startX = centerX + cos(startRadius) * doughnutRadius,
+          startY = centerY + sin(startRadius) * doughnutRadius,
+          endX2 = centerX + cos(startRadius) * cutoutRadius,
+          endY2 = centerY + sin(startRadius) * cutoutRadius,
+          endX = centerX + cos(endRadius) * doughnutRadius,
+          endY = centerY + sin(endRadius) * doughnutRadius,
+          startX2 = centerX + cos(endRadius) * cutoutRadius,
+          startY2 = centerY + sin(endRadius) * cutoutRadius;
+      var cmd = [
+        'M', startX, startY,//Move pointer
+        'A', doughnutRadius, doughnutRadius, 0, largeArc, 1, endX, endY,//Draw outer arc path
+        'L', startX2, startY2,//Draw line path(this line connects outer and innner arc paths)
+        'A', cutoutRadius, cutoutRadius, 0, largeArc, 0, endX2, endY2,//Draw inner arc path
+        'Z'//Cloth path
+      ];
+      $paths[i].setAttribute("d", cmd.join(' '));
+      startRadius += segmentAngle;
+    }
+  }
+  function drawDoughnutText(animationDecimal, segmentTotal) {
+    $summaryNumber.style.opacity = animationDecimal;
+    $summaryNumber.textContent = (segmentTotal * animationDecimal).toFixed(1);
+    var tmpNumber = settings.shortInt ? shortKInt(segmentTotal) : segmentTotal;
+    $summaryNumber.innerHTML = tmpNumber;
+    $summaryNumber.style.fontSize = getScaleFontSize( $summaryNumber, tmpNumber);
+  }
+  function animateFrame(cnt, drawData) {
+    var easeAdjustedAnimationPercent =(settings.animation)? CapValue(easingFunction(cnt), null, 0) : 1;
+    drawData(easeAdjustedAnimationPercent);
+  }
+  function animationLoop(drawData) {
+    var animFrameAmount = (settings.animation)? 1 / CapValue(settings.animationSteps, Number.MAX_VALUE, 1) : 1,
+        cnt =(settings.animation)? 0 : 1;
+    requestAnimFrame(function() {
+      cnt += animFrameAmount;
+      animateFrame(cnt, drawData);
+      if (cnt <= 1) {
+        requestAnimFrame(arguments.callee);
+      } else {
+        settings.afterDrawed.call($this); //todo
       }
-      for (var i = 0, len = data.length; i < len; i++) {
-        var segmentAngle = rotateAnimation * ((data[i].value / segmentTotal) * (PI * 2)),
-            endRadius = startRadius + segmentAngle,
-            largeArc = ((endRadius - startRadius) % (PI * 2)) > PI ? 1 : 0,
-            startX = centerX + cos(startRadius) * doughnutRadius,
-            startY = centerY + sin(startRadius) * doughnutRadius,
-            endX2 = centerX + cos(startRadius) * cutoutRadius,
-            endY2 = centerY + sin(startRadius) * cutoutRadius,
-            endX = centerX + cos(endRadius) * doughnutRadius,
-            endY = centerY + sin(endRadius) * doughnutRadius,
-            startX2 = centerX + cos(endRadius) * cutoutRadius,
-            startY2 = centerY + sin(endRadius) * cutoutRadius;
-        var cmd = [
-          'M', startX, startY,//Move pointer
-          'A', doughnutRadius, doughnutRadius, 0, largeArc, 1, endX, endY,//Draw outer arc path
-          'L', startX2, startY2,//Draw line path(this line connects outer and innner arc paths)
-          'A', cutoutRadius, cutoutRadius, 0, largeArc, 0, endX2, endY2,//Draw inner arc path
-          'Z'//Cloth path
-        ];
-        $paths[i].attr("d", cmd.join(' '));
-        startRadius += segmentAngle;
-      }
-    }
-    function drawDoughnutText(animationDecimal, segmentTotal) {
-      $summaryNumber
-        .css({opacity: animationDecimal})
-        .text((segmentTotal * animationDecimal).toFixed(1));
-	  var tmpNumber = settings.shortInt ? shortKInt(segmentTotal) : segmentTotal;
-	  $summaryNumber.html(tmpNumber).css('font-size', getScaleFontSize( $summaryNumber, tmpNumber));
-    }
-    function animateFrame(cnt, drawData) {
-      var easeAdjustedAnimationPercent =(settings.animation)? CapValue(easingFunction(cnt), null, 0) : 1;
-      drawData(easeAdjustedAnimationPercent);
-    }
-    function animationLoop(drawData) {
-      var animFrameAmount = (settings.animation)? 1 / CapValue(settings.animationSteps, Number.MAX_VALUE, 1) : 1,
-          cnt =(settings.animation)? 0 : 1;
-      requestAnimFrame(function() {
-          cnt += animFrameAmount;
-          animateFrame(cnt, drawData);
-          if (cnt <= 1) {
-            requestAnimFrame(arguments.callee);
-          } else {
-            settings.afterDrawed.call($this);
-          }
-      });
-    }
-    function Max(arr) {
-      return Math.max.apply(null, arr);
-    }
-    function Min(arr) {
-      return Math.min.apply(null, arr);
-    }
-    function isNumber(n) {
-      return !isNaN(parseFloat(n)) && isFinite(n);
-    }
-    function CapValue(valueToCap, maxValue, minValue) {
-      if (isNumber(maxValue) && valueToCap > maxValue) return maxValue;
-      if (isNumber(minValue) && valueToCap < minValue) return minValue;
-      return valueToCap;
-    }
-    function shortKInt (int) {
-		int = int.toString();
-		var strlen = int.length;
-		if(strlen<5)
-			return int;
-		if(strlen<8)
-			return '<span title="' +  int +  '">' + int.substring(0, strlen-3) + 'K</span>';
-		return '<span title="' + int  + '">' + int.substring( 0, strlen-6) + 'M</span>';
-	}
-	function getScaleFontSize(block, newText) {
-		block.css('font-size', '');
+    });
+  }
+  function Max(arr) {
+    return Math.max.apply(null, arr);
+  }
+  function Min(arr) {
+    return Math.min.apply(null, arr);
+  }
+  function isNumber(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  }
+  function CapValue(valueToCap, maxValue, minValue) {
+    if (isNumber(maxValue) && valueToCap > maxValue) return maxValue;
+    if (isNumber(minValue) && valueToCap < minValue) return minValue;
+    return valueToCap;
+  }
+  function shortKInt (int) {
+    int = int.toString();
+    var strlen = int.length;
+    if(strlen<5)
+      return int;
+    if(strlen<8)
+      return '<span title="' +  int +  '">' + int.substring(0, strlen-3) + 'K</span>';
+    return '<span title="' + int  + '">' + int.substring( 0, strlen-6) + 'M</span>';
+  }
+  function getScaleFontSize(block, newText) {
+    block.style.fontSize = '';
+    newText = newText.toString().replace(/(<([^>]+)>)/ig,"");
+    var newFontSize = parseInt(block.clientWidth) / newText.length * settings.ratioFont;
+    // Not very good : http://stephensite.net/WordPressSS/2008/02/19/how-to-calculate-the-character-width-accross-fonts-and-points/
+    // But best quick way the 1.5 number is to affinate in function of the police
+    var maxCharForDefaultFont = parseInt(block.clientWidth) - newText.length * parseInt(block.style.fontSize) / settings.ratioFont;
+    if(maxCharForDefaultFont<0)
+      return newFontSize+'px';
+    else
+      return '';
+  }
+  /**
+  function getScaleFontSize(block, newText) {
+    block.css('font-size', '');
         newText = newText.toString().replace(/(<([^>]+)>)/ig,"");
-		var newFontSize = block.width() / newText.length * settings.ratioFont;
-		// Not very good : http://stephensite.net/WordPressSS/2008/02/19/how-to-calculate-the-character-width-accross-fonts-and-points/
-		// But best quick way the 1.5 number is to affinate in function of the police
-		var maxCharForDefaultFont = block.width() - newText.length * block.css('font-size').replace(/px/, '') / settings.ratioFont;
-		if(maxCharForDefaultFont<0)
-			return newFontSize+'px';
-		else
-			return '';
-	}
-	/**
-	function getScaleFontSize(block, newText) {
-		block.css('font-size', '');
-        newText = newText.toString().replace(/(<([^>]+)>)/ig,"");
-		var newFontSize = block.width() / newText.length;
-		if(newFontSize<block.css('font-size').replace(/px/, ''))
-			return newFontSize+'px';
-		else
-			return '';
-	}*/
+    var newFontSize = block.width() / newText.length;
+    if(newFontSize<block.css('font-size').replace(/px/, ''))
+      return newFontSize+'px';
+    else
+      return '';
+  }*/
     return $this;
   };
-})(jQuery);
+  drawDoughnutChart([
+    { title: "Tokyo",         value : 100,  color: "#2C3E50" },
+    { title: "San Francisco", value:  80,   color: "#FC4349" },
+    { title: "New York",      value:  70,   color: "#6DBCDB" },
+    { title: "London",        value : 50,   color: "#F7E248" },
+    { title: "Sydney",        value : 40,   color: "#D7DADB" },
+    { title: "Berlin",        value : 20,   color: "#FFF" }
+  ]);
